e3e07fa25d5082e8a0e45f6c47ca3cc82f72636f
Azikzz1 <aziretshaipov@gmail.com>
2024-12-28 03:19:30 +0600

    Control Work!

---
+++ db/main_db.py
@@ -0,0 +1,27 @@
+import sqlite3
+from db import queries
+
+db_store = sqlite3.connect('db/STORE.sqlite3')
+cursor = db_store.cursor()
+
+
+async def sql_insert_store_products(model_name, category, size_1, price, product_id, photo):
+    cursor.execute(queries.INSERT_store_products_QUERY, (
+        model_name, category, size_1, price, product_id, photo
+    ))
+    db_store.commit()
+
+
+def get_db_connection():
+    return sqlite3.connect('db/STORE.sqlite3')
+
+
+def fetch_all_products():
+    conn = get_db_connection()
+    products = conn.execute("""
+        SELECT * FROM store_products
+    """).fetchall()
+    conn.close()
+    return products
+
+
